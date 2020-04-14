a916500404673c872a5483ac72f346f31f11dd5e
Ulvbjern <dimich14@gmail.com>
2020-04-14 15:23:37 +0300

    change realisation for readably

--- src/main/java/com/sberbot/app/service/BotService.java
+++ src/main/java/com/sberbot/app/service/BotService.java
@@ -1,25 +1,19 @@
 package com.sberbot.app.service;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import com.sberbot.app.dao.BotAppDao;
 import com.sberbot.app.model.AuctionModel;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
-
-import java.sql.Time;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.Set;
 
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -36,7 +30,6 @@ public class BotService {
 
     public void getAuction () {
         try {
-            //"C:/App/chromedriver.exe"
             System.setProperty("webdriver.chrome.driver", environment.getProperty("webdriver.path"));
             System.setProperty("selenide.browser", "Chrome");
             logger.info("Переходим на сайт сбербанк-аст");
@@ -44,44 +37,23 @@ public class BotService {
             element(byId("searchInput")).setValue("осаго").pressEnter();
             //element(byClassName("es-el-source-term")).shouldHave(text("Госзакупки по 44-ФЗ")).click();
             //element(byId("footerPagerSelect")).selectOptionContainingText("20");
-            //sleep(2000);
+
             SelenideElement selenideElement = element(byXpath("//*[@id='resultTable']")).shouldBe(Condition.visible);
 
             List<AuctionModel> auctionModels = new ArrayList<>();
             List<AuctionModel> auctionModelsFromDao = botAppDao.getAllAutions();
 
-            List<String> auctionCodes = selenideElement.findAll(byClassName("es-el-code-term")).texts();
+            getListOfCodes(selenideElement,auctionModels);
+            getListOfPublicDates(selenideElement,auctionModels);
+            getListOfOrgNames(selenideElement,auctionModels);
+            getListOfTenderNames(selenideElement,auctionModels);
+            getListOfTenderSums(selenideElement,auctionModels);
 
-            for(String auctionCode : auctionCodes) {
-                auctionModels.add(new AuctionModel(auctionCode,"null","null","null","null"));
-            }
+            Set<AuctionModel> uniqueAuctionModels = new HashSet<>(auctionModels);
+            Set<AuctionModel> uniqueAuctionModelsFromDao = new HashSet<>(auctionModelsFromDao);
 
-            List<String> publicDates = selenideElement.findAll(byCssSelector("span[content='leaf:PublicDate']")).texts();
-
-            for(int i=0; i < publicDates.size(); i ++) {
-                auctionModels.get(i).setPublicDate(publicDates.get(i));
-            }
-
-            List<String> orgNames = selenideElement.findAll(byClassName("es-el-org-name")).texts();
-
-            for(int i=0; i < orgNames.size();i++) {
-                auctionModels.get(i).setOrgName(orgNames.get(i));
-            }
-
-            List<String> tenderNames = selenideElement.findAll(byClassName("es-el-name")).texts();
-
-            for(int i=0; i < tenderNames.size();i++) {
-                auctionModels.get(i).setTenderName(tenderNames.get(i));
-            }
-
-            List<String> tenrersSums = selenideElement.findAll(byClassName("es-el-amount")).texts();
-
-            for (int i=0;i < tenrersSums.size(); i++) {
-                auctionModels.get(i).setSum(tenrersSums.get(i));
-            }
-
-            for (AuctionModel auctionModel : auctionModels) {
-                if(!auctionModelsFromDao.contains(auctionModel)) {
+            for (AuctionModel auctionModel : uniqueAuctionModels) {
+                if(!uniqueAuctionModelsFromDao.contains(auctionModel)) {
                     if(auctionModel.getTenderName().contains("ОСАГО")) {
                         logger.info("Загружаем в базу данных новый аукцион");
                         botAppDao.addAuction(auctionModel);
@@ -103,4 +75,46 @@ public class BotService {
             logger.info("Закрыли окно браузера");
         }
     }
+
+    private List<AuctionModel> getListOfCodes(SelenideElement selenideElement, List<AuctionModel> auctionModels) {
+        List<String> auctionCodes = selenideElement.findAll(byClassName("es-el-code-term")).texts();
+        for(String auctionCode : auctionCodes) {
+            auctionModels.add(new AuctionModel(auctionCode,"null","null","null","null"));
+        }
+        return auctionModels;
+    }
+
+    private List<AuctionModel> getListOfPublicDates(SelenideElement selenideElement, List<AuctionModel> auctionModels) {
+        List<String> publicDates = selenideElement.findAll(byCssSelector("span[content='leaf:PublicDate']")).texts();
+        for(int i=0; i < publicDates.size(); i ++) {
+            auctionModels.get(i).setPublicDate(publicDates.get(i));
+        }
+        return auctionModels;
+    }
+
+    private List<AuctionModel> getListOfOrgNames(SelenideElement selenideElement,List<AuctionModel> auctionModels) {
+        List<String> orgNames = selenideElement.findAll(byClassName("es-el-org-name")).texts();
+        for(int i=0; i < orgNames.size();i++) {
+            auctionModels.get(i).setOrgName(orgNames.get(i));
+        }
+
+        return auctionModels;
+    }
+
+    private List<AuctionModel> getListOfTenderNames(SelenideElement selenideElement,List<AuctionModel> auctionModels) {
+        List<String> tenderNames = selenideElement.findAll(byClassName("es-el-name")).texts();
+        for(int i=0; i < tenderNames.size();i++) {
+            auctionModels.get(i).setTenderName(tenderNames.get(i));
+        }
+        return auctionModels;
+    }
+
+    private List<AuctionModel> getListOfTenderSums(SelenideElement selenideElement,List<AuctionModel> auctionModels) {
+        List<String> tenrersSums = selenideElement.findAll(byClassName("es-el-amount")).texts();
+        for (int i=0;i < tenrersSums.size(); i++) {
+            auctionModels.get(i).setSum(tenrersSums.get(i));
+        }
+        return auctionModels;
+    }
+
 }
