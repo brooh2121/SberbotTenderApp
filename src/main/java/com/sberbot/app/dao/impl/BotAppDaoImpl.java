94fb0d9c074191cd6460b037bd74fd0fea1f9fc9
Ulvbjern <dimich14@gmail.com>
2020-04-17 09:45:06 +0300

    Add DataSourceConfig for multiple DB connections

--- src/main/java/com/sberbot/app/dao/impl/BotAppDaoImpl.java
+++ src/main/java/com/sberbot/app/dao/impl/BotAppDaoImpl.java
@@ -5,6 +5,7 @@ import com.sberbot.app.dao.BotAppDao;
 import com.sberbot.app.dao.mapper.AuctionsMapper;
 import com.sberbot.app.model.AuctionModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +15,8 @@ import java.util.List;
 public class BotAppDaoImpl implements BotAppDao {
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    @Qualifier("jdbcTemplatePostgreTend")
+    JdbcTemplate jdbcTemplatePostgreTend;
 
 
     @Override
@@ -26,20 +28,20 @@ public class BotAppDaoImpl implements BotAppDao {
                 "'"+auctionModel.getPublicDate()+"',\n" +
                 "'"+auctionModel.getSum()+"'\n" +
                  ")";
-        jdbcTemplate.update(query);
+        jdbcTemplatePostgreTend.update(query);
         return auctionModel;
     }
 
     @Override
     public List<AuctionModel> getAllAutions() {
         String query = "select * from public.auctions a order by to_timestamp(publication_date,'dd.mm.yyyy hh24:mi:ss') desc limit 20";
-        return jdbcTemplate.query(query,new AuctionsMapper());
+        return jdbcTemplatePostgreTend.query(query,new AuctionsMapper());
     }
 
     @Override
     public String getMaxPublicDate() {
         String query = "select max(to_timestamp(publication_date,'DD.MM.YYYY HH24:MI')) from auctions";
-        String strResult = jdbcTemplate.queryForObject(query,String.class);
+        String strResult = jdbcTemplatePostgreTend.queryForObject(query,String.class);
         return strResult;
     }
 
